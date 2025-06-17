04b76cf8f95eb6ddee7c4a0e0955b44d1bf07ac6
unblock-eve <coke@dr.com>
2025-06-17 07:34:53 -0500

    Update SETTINGS.lua

--- SETTINGS.lua
+++ SETTINGS.lua
@@ -2,20 +2,16 @@
 return {
     ENABLE_AGE_FILTER    = true,
     ENABLE_WEIGHT_FILTER = true,
-    MIN_AGE              = 30,
-    MIN_WEIGHT           = 9,
+    MIN_AGE              = 40,
+    MIN_WEIGHT           = 15,
 
     ALLOWED_NAMES = {
-        ["moth"]           = true,
         ["queen bee"]      = true,
         ["racoon"]         = true,
-        ["brown mouse"]    = true,
         ["dragonfly"]    = true,
         ["Polar Bear"]    = true,
         ["chicken zombie"]    = true,
         ["Disco Bee"]    = true,
-        ["cooked owl"]    = true,
-        ["pack bee"]    = true,
         
     },
 
