aed5b197508ba5ea2f4fdacc7e10e0c54f2da759
unblock-eve <coke@dr.com>
2025-06-17 05:33:54 -0500

    Create allowed.lua

---
+++ allowed.lua
@@ -0,0 +1,9 @@
+local ALLOWED_NAMES = {
+	["tarantula hawk"] = true,
+	["bee"]            = true,
+	["moth"]       = true,
+	["queen bee"]         = true,
+	["grey mouse"]         = true,
+	["racoon"]         = true,
+	["brown mouse"]         = true,
+}
