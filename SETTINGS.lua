dfe4a277dc260126970cef5c5583ebf1866419f3
unblock-eve <coke@dr.com>
2025-06-20 07:26:24 -0500

    Update SETTINGS.lua

--- SETTINGS.lua
+++ SETTINGS.lua
@@ -11,6 +11,7 @@ return {
         ["disco bee"]    = true,
         ["butterfly"]    = true,
         ["firefly"]      =  true,
+        ["chicken zombie"]      =  true,
     },
     DELAY_BETWEEN_PETS = 0.125,
 
