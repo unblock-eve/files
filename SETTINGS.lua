87cbfcc245f98208dd0f9c9f51afdb7e79b4cc9f
unblock-eve <coke@dr.com>
2025-06-23 21:18:34 -0500

    Update SETTINGS.lua

--- SETTINGS.lua
+++ SETTINGS.lua
@@ -7,7 +7,7 @@ return {
 
     ALLOWED_NAMES = {
         ["raccoon"]         = true,
-        ["ant"]    = true,
+        ["squirrel"]    = true,
         ["disco bee"]    = true,
         ["butterfly"]    = true,
         ["mimic octopus"]      =  true,
