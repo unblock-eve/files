7294bf2b3eda31bfdd3ce53f1f6f8634d0ddd9f0
unblock-eve <coke@dr.com>
2025-07-18 02:16:50 -0500

    Update SETTINGS.lua

--- SETTINGS.lua
+++ SETTINGS.lua
@@ -14,6 +14,7 @@ return {
         ["butterfly"]     = true,
         ["mimic octopus"] = true,
         ["dragonfly"]     = true,
+        ["dilophosaurus"] = true,
     },
 
     ALLOWED_MUTATIONS = {
