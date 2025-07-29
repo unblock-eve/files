8051454f180f820a5cd530683925a98e3ee2fbc3
unblock-eve <coke@dr.com>
2025-07-29 14:00:14 -0500

    Update SETTINGS.lua

--- SETTINGS.lua
+++ SETTINGS.lua
@@ -30,7 +30,6 @@ return {
         ["disco bee"]     = true,
         ["kitsune"] = true,
         ["fennec fox"] = true,
-        --["corrupted kitsune"] = true,
         ["chicken zombie"] = true,
     },
 
